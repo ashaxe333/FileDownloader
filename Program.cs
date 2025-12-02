877ce09015f91b509bdeb81319736c5b610ca30d
ashaxe333 <h.faktor06@gmail.com>
2025-12-02 22:06:34 +0100

    added while loop
    While loop with 2 options for user has been added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter url adress of file to download: ");
-            string url = Console.ReadLine();
-            Downloader d = new Downloader(url);
+            bool repeat = true;
+            Downloader d;
+            while (repeat)
+            {
+                Console.WriteLine("1 - download\n2 - exit");
+                string mode = Console.ReadLine();
+                switch (mode) {
+                    case "1":
+                        Console.WriteLine("Enter url adress of file to download: ");
+                        string url = Console.ReadLine();
+                        d = new Downloader(url);
+                        break;
+                    case "2":
+                        repeat = false;
+                        break;
+                    default:
+                        throw new Exception("Invalid input");
+                }
+                
+            }
         }
     }
 }
